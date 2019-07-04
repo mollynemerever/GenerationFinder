43bbfd31210e08a6c42db36c367f28d421034ee1
github username <github email address>
2019-07-04 07:24:58 -0700

    genx_facts

--- src/components/genx.js
+++ src/components/genx.js
@@ -3,10 +3,50 @@ import React, { Component } from "react";
 export default class GenX extends Component {
   render() {
     return (
-      <div>
-        {" "}
-        Gen X 1965-1976
-        <button onClick={e => this.props.updateYear(e, null)}>back</button>
+      <div className="postboomer">
+        <div className="facts">
+          <h1>Post Boomers</h1>
+          <h2> Generation X</h2>
+          <h4> 1965 - 1976</h4>
+          <br />
+          <br />
+          <li>
+            {" "}
+            <strong>Core Values:</strong> Independent, Pragmatism, Technology
+            Literate, Entrepreneurial{" "}
+          </li>{" "}
+          <br />
+          <li>
+            {" "}
+            <strong> Upbringing: </strong>Political Scandals, USA loosing power,
+            Lay Offs, Dual Income Families
+          </li>
+          <br />
+          <li>
+            {" "}
+            <strong>Atrributes: </strong>Sociable, Highly Communicative,
+            Optimistic, Global Mindset{" "}
+          </li>
+          <br />
+          <li>
+            {" "}
+            <strong>Work Ethic: </strong>Adaptable, Antiestablishment Mentality,
+            Results Focused
+          </li>
+          <br />
+          <li>
+            {" "}
+            <strong>Technology: </strong> Is Familiar, Cell Phone, Pager
+          </li>
+          <br />
+          <li>
+            {" "}
+            <strong>Celebrities: </strong>Jenifer Aniston, Leonardo DiCaprio,
+            Will Smith, Amy Poehler{" "}
+          </li>
+          <br />
+          <button onClick={e => this.props.updateYear(e, null)}>back</button>
+        </div>
       </div>
     );
   }
